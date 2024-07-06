1d1d8f663f47f7313eff89d581355e45c6b66f27
Jesus Molina <netod90@gmail.com>
2024-07-06 14:58:03 -0700

    Chore 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,13 +15,16 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
+  const total = good + neutral + bad;
+  const average = (good * 1 + neutral * 0 + bad * -1) / total;
+  const positive = (good * 100) / total;
   const setToValue = (e) => {
     const btnName = e.target.innerText;
     if (btnName === 'Good') setGood(good + 1);
     if (btnName === 'Neutral') setNeutral(neutral + 1);
     if (btnName === 'Bad') setBad(bad + 1);
   };
+
   return (
     <>
       <h1>Give feedback</h1>
@@ -33,6 +36,9 @@ function App() {
       <Display score={good} text="Good" />
       <Display score={neutral} text="Neutral" />
       <Display score={bad} text="Bad" />
+      <Display score={total} text="All" />
+      <p>Average {average}</p>
+      <p>Positive {positive}%</p>
     </>
   );
 }
