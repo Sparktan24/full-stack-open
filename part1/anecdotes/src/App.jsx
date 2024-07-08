dff11cc1532bd3f64f139106b3f6f8f25ae0bbc4
Jesus Molina <netod90@gmail.com>
2024-07-08 16:05:45 -0700

    chore: Refactor App component to separate Anecdotes component

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,20 +1,7 @@
 import { useState } from 'react';
 
-function App() {
-  const [selected, setSelected] = useState(0);
-  const points = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0 };
-  const [votes, setVotes] = useState(points);
+function Anecdotes({ anecdotes, selected, setSelected, votes, setVotes }) {
   const [mostVoted, setMostVoted] = useState('');
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.',
-  ];
 
   const handleClick = () => {
     const randomGenerator = Math.floor(anecdotes.length * Math.random());
@@ -25,11 +12,10 @@ function App() {
     const newVotes = { ...votes };
     newVotes[selected] += 1;
     setVotes(newVotes);
+    hasMostVotes(newVotes);
   };
 
-  const hasMostVotes = () => {
-    const newVotes = { ...votes };
-    //console.log(newVotes.length);
+  const hasMostVotes = (newVotes) => {
     let max = 0;
     let index = 0;
     for (const element in newVotes) {
@@ -38,17 +24,8 @@ function App() {
         max = aux;
         index = parseInt(element);
       }
-      //console.log(`${element}: ${newVotes[element]}`);
     }
     setMostVoted(anecdotes[index]);
-    console.log(anecdotes[index]);
-    //console.log('index', index);
-    //console.log('max', max);
-    //console.log(newVotes[index]);
-    //console.log(newVotes[max]);
-    console.log(newVotes);
-    //newVotes.sort((a, b) => b - a);
-    //console.log(newVotes);
   };
 
   return (
@@ -57,12 +34,36 @@ function App() {
       <div>{anecdotes[selected]}</div>
       <p>Has {votes[selected]} votes</p>
       <button onClick={handleVote}>vote</button>
-      {/* {console.log(votes)} */}
       <button onClick={handleClick}>Next anecdote</button>
-
       <h1>Anecdote with most votes</h1>
-      <button onClick={hasMostVotes}>Show most voted</button>
-      {mostVoted}
+      <p>{mostVoted}</p>
+    </>
+  );
+}
+
+function App() {
+  const [selected, setSelected] = useState(0);
+  const points = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0 };
+  const [votes, setVotes] = useState(points);
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.',
+  ];
+  return (
+    <>
+      <Anecdotes
+        anecdotes={anecdotes}
+        selected={selected}
+        setSelected={setSelected}
+        votes={votes}
+        setVotes={setVotes}
+      />
     </>
   );
 }
