bbb57c10a01b89dc021405d8cef4003eafc3092e
Jesus Molina <netod90@gmail.com>
2024-07-09 19:59:19 -0700

    Add chore 2.8

--- part2/2.6-tel-diary/src/App.jsx
+++ part2/2.6-tel-diary/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 
 function App() {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
   };
 
   const addPerson = (e) => {
@@ -16,8 +23,10 @@ function App() {
     } else {
       const personObject = {
         name: newName,
+        number: newNumber,
       };
       setNewName('');
+      setNewNumber('');
       setPersons(persons.concat(personObject));
     }
   };
@@ -30,13 +39,18 @@ function App() {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
 
       {persons.map((person, i) => (
-        <div key={i}>{person.name}</div>
+        <div key={i}>
+          {person.name} {person.number}
+        </div>
       ))}
     </div>
   );
