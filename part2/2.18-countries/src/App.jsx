382098eb2ce3b18a6a993e85745b79f44d187472
Jesus Molina <netod90@gmail.com>
2024-07-21 13:46:17 -0700

    Add chore 2.19

--- part2/2.18-countries/src/App.jsx
+++ part2/2.18-countries/src/App.jsx
@@ -1,11 +1,30 @@
 import { useEffect, useState } from 'react';
 import countryService from './services/country';
 
+function Country({ country }) {
+  return (
+    <div key={country.ccn3}>
+      <h1>{country.name.common}</h1>
+      <p>Capital {country.capital}</p>
+      <p>Area {country.area}Km2</p>
+      <h2>Languages:</h2>
+      <ul>
+        {Object.values(country.languages).map((lang) => (
+          <li key={lang}>{lang}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt={country.flags.alt} />
+    </div>
+  );
+}
 function App() {
   const [countries, setCountries] = useState([]);
   const [query, setQuery] = useState('');
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [notification, setNotification] = useState(null);
+  const [showButton, setShowButton] = useState(false);
+  const [showCountry, setShowCountry] = useState(null);
+  const [singleCountry, setSingleCountry] = useState([]);
   //const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all';
   useEffect(() => {
     countryService.getAllCountries().then((country) => setCountries(country));
@@ -22,16 +41,29 @@ function App() {
       setFilteredCountries([]);
       setNotification('too many matches, specify another filter');
     } else if (filterCountry.length <= 10 && filterCountry.length > 1) {
+      setShowButton(true);
       setFilteredCountries(filterCountry);
       setNotification(null);
     } else {
       setFilteredCountries(filterCountry);
+      //setShowButton(false);
+      setShowCountry(null);
+      setSingleCountry([]);
       setNotification(null);
     }
   };
 
   const handleInputChange = (e) => {
     setQuery(e.target.value);
+  };
+
+  const showCountryHandler = (e, country) => {
+    if (singleCountry.ccn3 === country.ccn3 && showCountry) {
+      setShowCountry(null);
+    } else {
+      setShowCountry(country.ccn3);
+      setSingleCountry(country);
+    }
   };
   return (
     <>
@@ -48,25 +80,19 @@ function App() {
 
       {filteredCountries.length === 1
         ? filteredCountries.map((country) => (
-            <div key={country.ccn3}>
-              <h1>{country.name.common}</h1>
-              <p>Capital {country.capital}</p>
-              <p>Area {country.area}Km2</p>
-              <h2>Languages:</h2>
-              <ul>
-                {Object.values(country.languages).map((lang) => (
-                  <li key={lang}>{lang}</li>
-                ))}
-              </ul>
-              <img src={country.flags.png} alt={country.flags.alt} />
-            </div>
+            <Country key={country.ccn3} country={country} />
           ))
         : filteredCountries.map((country) => (
             <div key={country.ccn3}>
               {country.name.common}
-              {console.log(filteredCountries.length)}
+              {showButton && (
+                <button onClick={(e) => showCountryHandler(e, country)}>
+                  {showCountry === country.ccn3 ? 'Hide' : 'Show'}
+                </button>
+              )}
             </div>
           ))}
+      <div>{showCountry && <Country country={singleCountry} />}</div>
     </>
   );
 }
---
+++ part2/2.18-countries/src/components/Countries.jsx
@@ -0,0 +1,5 @@
+function Countries() {
+  return <h1>Countries</h1>;
+}
+
+export default Countries;
